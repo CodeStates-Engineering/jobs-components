c6f8a3352c4964d34703af3a42aee59a4a499c88
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-02-02 15:50:15 +0900

    chore: canary 배포 스크립트 분리
    - canary 버전 설정 스크립트 분리

--- scripts/set-canary-version.sh
+++ scripts/set-canary-version.sh
@@ -4,6 +4,3 @@ git_commit_sha=`git rev-parse --short HEAD`
 
 
 npm run version $pkg_version-$git_commit_sha
-
-# npx npm@7 run deploy --workspaces -- --tag canary
-npm run deploy:package -- --tag canary
