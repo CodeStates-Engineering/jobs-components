21cd8220134bcaf2dad475620eb25c8d0285492d
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-02-09 20:36:55 +0900

    feat: Textbox 기능 추가
    - type 로직 추가

--- src/components/atoms/Textbox/index.tsx
+++ src/components/atoms/Textbox/index.tsx
@@ -1,45 +1,79 @@
 import { useComponentSelfState } from 'hooks/useComponentSelfState';
 
+import { useMemo } from 'react';
+
 import styles from './index.module.scss';
 
-export interface TextboxProps {
-  value?: string | number | readonly string[];
+type TextType = 'text' | 'number' | 'large-number' | 'password';
+
+type ChangeHandlerParams<T extends TextType> =
+  | (T extends 'number' | 'large-number' ? number : string)
+  | undefined;
+
+export interface TextboxProps<T extends TextType> {
+  value?: string | number;
   placeholder?: string;
   rightIcon?: React.ReactNode;
   onlyUpdatedByParent?: boolean;
-  onChange?: (value: string) => void;
+  onChange?: (value: ChangeHandlerParams<T>) => void;
   onFocus?: React.FocusEventHandler<HTMLInputElement>;
   width?: React.CSSProperties['width'];
-  validation?: (value: string) => string | undefined;
+  validation?: (value: ChangeHandlerParams<T>) => string | undefined;
+  type?: T;
 }
 
-export const Textbox = ({
-  value: originalValue,
+export const Textbox = <T extends TextType>({
+  value: originalValue = '',
   placeholder = '텍스트를 입력하세요.',
   rightIcon,
   onlyUpdatedByParent,
   onChange,
   onFocus,
   width = '300px',
-  validation,
-}: TextboxProps) => {
+  // validation,
+  type,
+}: TextboxProps<T>) => {
   const [value, setValue] = useComponentSelfState(
-    originalValue ? String(originalValue) : '',
+    originalValue,
     onlyUpdatedByParent,
   );
+
+  const convertValueForView: (value: string | number) => string | number =
+    useMemo(() => {
+      switch (type) {
+        case 'large-number':
+          return (value) =>
+            value === '' ? '' : Number(value).toLocaleString();
+        default:
+          return (value) => value;
+      }
+    }, [type]);
+
+  const convertValueForChangeHandler = useMemo(() => {
+    switch (type) {
+      case 'large-number':
+        return (value) =>
+          value === '' ? undefined : Number(value.replace(/[^0-9]/g, ''));
+      case 'number':
+        return (value) => (value === '' ? undefined : Number(value));
+      default:
+        return (value) => value || undefined;
+    }
+  }, [type]) as (value: string) => ChangeHandlerParams<T>;
 
   return (
     <div className={styles.textbox} style={{ width }}>
       <input
         className={styles['textbox-control']}
         onFocus={onFocus}
-        value={value}
+        value={convertValueForView(value)}
         placeholder={placeholder}
-        onChange={({ target: { value } }) => {
-          setValue?.(value);
+        onChange={(e) => {
+          const value = convertValueForChangeHandler(e.target.value);
+          setValue?.(value ?? '');
           onChange?.(value);
-          validation?.(value);
         }}
+        type={type ?? 'text'}
       />
       {rightIcon}
     </div>
