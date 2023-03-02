6383248afe316446300b9dd09c5eaa19b1f94e23
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-03-02 20:12:24 +0900

    JOBS-73: Hr 컴포넌트 개발

---
+++ src/components/atoms/Hr/index.tsx
@@ -0,0 +1,37 @@
+import { cleanClassName } from 'utils';
+
+import { useMemo } from 'react';
+
+import styles from './index.module.scss';
+
+export interface HrProps {
+  type?: 'vertical' | 'horizontal';
+  color?: 'purple-550' | 'bluish-gray-200';
+  weight?: 'thin' | 'medium' | 'thick';
+  size?: React.CSSProperties['width'] | React.CSSProperties['height'];
+  margin?: React.CSSProperties['margin'];
+}
+
+export const Hr = ({
+  type = 'horizontal',
+  color = 'bluish-gray-200',
+  weight = 'thin',
+  size = '100%',
+  margin,
+}: HrProps) => {
+  const hrProps = useMemo(
+    () => ({
+      className: cleanClassName(
+        `${styles.hr} ${styles[`type-${type}`]} ${styles[`weight-${weight}`]} ${
+          styles[`color-${color}`]
+        }`,
+      ),
+      style: {
+        [type === 'horizontal' ? 'width' : 'height']: size,
+        margin,
+      },
+    }),
+    [type, color, weight, size, margin],
+  );
+  return type === 'horizontal' ? <hr {...hrProps} /> : <div {...hrProps} />;
+};
---
+++ src/components/atoms/Hr/stories/index.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Hr } from '..';
+
+import type { HrProps } from '..';
+
+export default {
+  title: 'atoms/Hr',
+  component: Hr,
+} as ComponentMeta<typeof Hr>;
+
+const HrStory: ComponentStory<typeof Hr> = (args) => (
+  <div
+    style={{
+      height: '50vh',
+      width: '100%',
+      padding: '20px',
+    }}
+  >
+    <Hr {...args} />
+  </div>
+);
+
+export const Default = HrStory.bind({});
+const defaultArgs: HrProps = {
+  type: 'horizontal',
+};
+Default.args = defaultArgs;
