c13a0de7a00b22f07b82fa7ca830bf821800ab34
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-03-03 16:52:23 +0900

    feat(Category): 컴포넌트 개발

--- src/components/molecules/Category/index.tsx
+++ src/components/molecules/Category/index.tsx
@@ -1,9 +1,11 @@
-import { Button } from 'components/atoms';
+import { Button, Hr } from 'components/atoms';
 import { Compatibility } from 'plugins';
+
+import type { CSSProperties } from 'react';
 
 import styles from './index.module.scss';
 
-import type { ButtonProps } from 'components/atoms';
+import type { ButtonProps, HrProps } from 'components/atoms';
 
 interface CategoryItem {
   label: string;
@@ -13,25 +15,54 @@ interface CategoryItem {
 export interface CategoryProps {
   items?: CategoryItem[];
   color?: ButtonProps['theme'];
+  bottomLineWeight?: HrProps['weight'];
   selectedColor?: ButtonProps['theme'];
+  selectedLineWeight?: HrProps['weight'];
+  selectedLineColor?: HrProps['color'];
+  width?: CSSProperties['width'];
+  height?: CSSProperties['height'];
 }
 
-export const Category = ({ items, color, selectedColor }: CategoryProps) => {
+export const Category = ({
+  items,
+  color,
+  selectedColor,
+  bottomLineWeight,
+  selectedLineWeight = 'medium',
+  selectedLineColor = 'purple-550',
+  width = '100%',
+  height = '100%',
+}: CategoryProps) => {
+  selectedColor = selectedColor ?? color;
   const { pathname } = Compatibility.useLocation();
   return (
-    <ul className={styles.category}>
-      {items?.map(({ label, to }, index) => (
-        <li key={index}>
-          <Compatibility.Link to={to}>
-            <Button
-              themeType="ghost"
-              theme={to === pathname ? color : selectedColor}
-            >
-              {label}
-            </Button>
-          </Compatibility.Link>
-        </li>
-      ))}
-    </ul>
+    <nav style={{ width, height }}>
+      <ul className={styles.category}>
+        {items?.map(({ label, to }, index) => {
+          const isSelected = to === pathname;
+          return (
+            <li key={index}>
+              <div className={styles['cateogry-link-wrap']}>
+                <Compatibility.Link to={to}>
+                  <Button
+                    themeType="ghost"
+                    theme={isSelected ? color : selectedColor}
+                    focusOutline={false}
+                  >
+                    {label}
+                  </Button>
+                </Compatibility.Link>
+              </div>
+              {isSelected && (
+                <div className={styles['selected-line-wrap']}>
+                  <Hr weight={selectedLineWeight} color={selectedLineColor} />
+                </div>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+      {bottomLineWeight ? <Hr weight={bottomLineWeight} /> : null}
+    </nav>
   );
 };
