8f597a560da2f5f40912e55b0395f272831efa02
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-03-06 19:56:54 +0900

    feat(LocalModal): float 관련 prop 추가

--- src/components/atoms/LocalModal/index.tsx
+++ src/components/atoms/LocalModal/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
+import type { CSSProperties } from 'react';
 
 import styles from './index.module.scss';
 import { Compatibility } from '../../../plugins';
@@ -7,9 +8,20 @@ import { cleanClassName } from '../../../utils';
 export interface LocalModalProps {
   opened?: boolean;
   children?: React.ReactNode;
+  top?: CSSProperties['top'];
+  left?: CSSProperties['left'];
+  right?: CSSProperties['right'];
+  bottom?: CSSProperties['bottom'];
 }
 
-export const LocalModal = ({ opened = false, children }: LocalModalProps) => {
+export const LocalModal = ({
+  opened = false,
+  children,
+  top,
+  left,
+  right,
+  bottom,
+}: LocalModalProps) => {
   const [openState, setOpenState] = useState<boolean | 'closing'>(opened);
   Compatibility.useLayoutEffect(() => {
     if (!opened) {
@@ -31,8 +43,14 @@ export const LocalModal = ({ opened = false, children }: LocalModalProps) => {
     }
   }, [openState]);
 
+  const floatStyle = useMemo(
+    () => ({ top, left, right, bottom }),
+    [top, left, right, bottom],
+  );
+
   return openState ? (
     <div
+      style={floatStyle}
       className={cleanClassName(
         `${styles['local-modal']} ${openState === 'closing' && styles.closing}`,
       )}
