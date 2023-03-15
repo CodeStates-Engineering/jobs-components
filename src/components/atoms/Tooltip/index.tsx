d32a9feee0d380dc9b9f4ad6e258cc3b6a9f04e2
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-03-15 14:23:09 +0900

    feat(Tooltip): 툴팁 컴포넌트 개발

---
+++ src/components/atoms/Tooltip/index.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+
+import styles from './index.module.scss';
+import { cleanClassName } from '../../../utils';
+
+export interface TooltipProps {
+  float?: 'top' | 'bottom';
+  children?: React.ReactNode;
+  message?: string;
+}
+
+export const Tooltip = ({
+  float = 'bottom',
+  children,
+  message,
+}: TooltipProps) => {
+  const [displayed, setDisplayed] = useState<'closing' | boolean>(false);
+  const [hovered, setHovered] = useState(false);
+
+  useEffect(() => {
+    if (displayed === 'closing') {
+      const closeTimer = setTimeout(() => setDisplayed(false), 300);
+      return () => clearTimeout(closeTimer);
+    }
+  }, [displayed]);
+
+  useEffect(() => {
+    if (hovered) {
+      setDisplayed(true);
+    } else {
+      const hoverOutTimer = setTimeout(() => setDisplayed('closing'), 100);
+      return () => clearTimeout(hoverOutTimer);
+    }
+  }, [hovered]);
+
+  return (
+    <div
+      className={styles['tooltip-container']}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <div className={styles['tooltip-opener']}>{children}</div>
+      {displayed && (
+        <div
+          onMouseEnter={() => {
+            if (displayed === 'closing') {
+              setHovered(false);
+            }
+          }}
+          className={cleanClassName(
+            `${styles['tooltip-message-container']} ${
+              styles[`float-${float}`]
+            } ${displayed === 'closing' && styles.closing}`,
+          )}
+        >
+          <div className={styles.triangle} />
+          <div className={styles['tooltip-message-wrap']}>{message}</div>
+        </div>
+      )}
+    </div>
+  );
+};
