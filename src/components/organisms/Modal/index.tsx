8c9dd796820941cf97494ad1fba98e71a0caf285
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-03-21 13:23:29 +0900

    chore: lint 실행

--- src/components/organisms/Modal/index.tsx
+++ src/components/organisms/Modal/index.tsx
@@ -1,12 +1,12 @@
-/* eslint-disable */
+import { X } from 'react-feather';
+
 import styles from './index.module.scss';
 import { cleanClassName } from '../../../utils';
 import { Button, FocusLayer } from '../../atoms';
 import { TabMenu } from '../../molecules';
-import type { TabMenuProps } from '../../molecules';
 
 import type { FocusLayerProps } from '../../atoms';
-import { X } from 'react-feather';
+import type { TabMenuProps } from '../../molecules';
 
 export interface ModalProps
   extends Pick<FocusLayerProps, 'blur' | 'className' | 'children'> {
@@ -25,38 +25,34 @@ export const Modal = ({
   onClose,
   children,
   footer,
-}: ModalProps) => {
-  return (
-    <FocusLayer focused={opened} onClick={onClose} blur={blur}>
-      {opened ? (
-        <article
-          className={cleanClassName(
-            `${styles['modal-container']} ${className}`,
-          )}
-        >
-          <header className={styles['modal-header']}>
-            <TabMenu
-              items={tabItems}
-              className={styles['tab-menu']}
-              fontSize="medium"
-              selectedColor="bluish-gray-800"
-              fontWeight={700}
-              bottomLineWeight="none"
-            />
-            <Button
-              icon={<X />}
-              themeType="ghost"
-              theme="bluish-gray-800"
-              size="small"
-              onClick={onClose}
-            />
-          </header>
-          <section className={styles['modal-content']}>{children}</section>
-          {footer ? (
-            <footer className={styles['modal-footer']}>{footer}</footer>
-          ) : null}
-        </article>
-      ) : null}
-    </FocusLayer>
-  );
-};
+}: ModalProps) => (
+  <FocusLayer focused={opened} onClick={onClose} blur={blur}>
+    {opened ? (
+      <article
+        className={cleanClassName(`${styles['modal-container']} ${className}`)}
+      >
+        <header className={styles['modal-header']}>
+          <TabMenu
+            items={tabItems}
+            className={styles['tab-menu']}
+            fontSize="medium"
+            selectedColor="bluish-gray-800"
+            fontWeight={700}
+            bottomLineWeight="none"
+          />
+          <Button
+            icon={<X />}
+            themeType="ghost"
+            theme="bluish-gray-800"
+            size="small"
+            onClick={onClose}
+          />
+        </header>
+        <section className={styles['modal-content']}>{children}</section>
+        {footer ? (
+          <footer className={styles['modal-footer']}>{footer}</footer>
+        ) : null}
+      </article>
+    ) : null}
+  </FocusLayer>
+);
