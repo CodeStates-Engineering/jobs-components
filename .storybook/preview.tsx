1dfe3724a3dff80a2d2ca0d00cbbb8eba111a2d8
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-05-12 14:22:30 +0900

    chore: vite 설정

---
+++ .storybook/preview.tsx
@@ -0,0 +1,33 @@
+import '../src/styles/_global.scss';
+import '../src/styles/storybook.scss';
+import React from 'react';
+
+import { MemoryRouter } from 'react-router-dom';
+import { Compatibility } from '../src/plugins';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+Compatibility.injectDependencies('react', {
+  location: useLocation,
+  link: Link,
+});
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <MemoryRouter>
+      <div style={{ display: 'flex', gap: '10px', flexWrap: 'wrap' }}>
+        <Story />
+      </div>
+    </MemoryRouter>
+  ),
+];
