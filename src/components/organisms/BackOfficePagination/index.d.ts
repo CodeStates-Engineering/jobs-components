a06da118b5ad9f16aed7ea3269fb90f8a47a7cd4
HyeokjaeLee <HyeokjaeLee@users.noreply.github.com>
2023-05-23 08:48:11 +0000

    deploy: 1fb18ff360288b7b9b9fe1a5892a46e8793cb8ed

--- src/components/organisms/BackOfficePagination/index.d.ts
+++ src/components/organisms/BackOfficePagination/index.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="react" />
 import type { PaginationProps } from '../../molecules';
-interface BackOfficePaginationProps extends Omit<PaginationProps, 'type' | 'displayedCount'> {
+export interface BackOfficePaginationProps extends Omit<PaginationProps, 'type' | 'displayedCount'> {
     onChangeItemsPerPage?: (itemsPerPage?: number) => void;
     itemPerPageOptions?: number[];
 }
 export declare const BackOfficePagination: ({ itemsPerPage, onChangeItemsPerPage, itemPerPageOptions, ...paginationProps }: BackOfficePaginationProps) => JSX.Element;
-export {};
