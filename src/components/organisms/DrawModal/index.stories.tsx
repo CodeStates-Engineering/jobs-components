8add636b44ed7925f0966f1158a9d29a6e42ca09
genuine-seok <genuineseok@gmail.com>
2023-05-24 09:44:50 +0900

    feat: direction 관련 stories 추가

--- src/components/organisms/DrawModal/index.stories.tsx
+++ src/components/organisms/DrawModal/index.stories.tsx
@@ -13,6 +13,10 @@ const meta: Meta<typeof DrawModal> = {
     (Story) => (
       <div
         style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '1rem',
           height: '100vh',
           width: '100%',
         }}
@@ -45,6 +49,56 @@ export const Default: Story = {
           onClose={() => {
             setOpened(false);
           }}
+        >
+          <DrawModal.Header>HEADER</DrawModal.Header>
+          <DrawModal.Body>BODY</DrawModal.Body>
+          <DrawModal.Footer>FOOTER</DrawModal.Footer>
+        </DrawModal>
+      </>
+    );
+  },
+};
+
+export const Direction: Story = {
+  render: (args) => {
+    const [leftOpened, setLeftOpened] = useState(false);
+    const [rightOpened, setRightOpened] = useState(false);
+
+    return (
+      <>
+        <Button
+          onClick={() => {
+            setLeftOpened(true);
+          }}
+        >
+          From Left
+        </Button>
+        <Button
+          onClick={() => {
+            setRightOpened(true);
+          }}
+        >
+          From Right
+        </Button>
+        <DrawModal
+          {...args}
+          opened={leftOpened}
+          onClose={() => {
+            setLeftOpened(false);
+          }}
+          direction="left"
+        >
+          <DrawModal.Header>HEADER</DrawModal.Header>
+          <DrawModal.Body>BODY</DrawModal.Body>
+          <DrawModal.Footer>FOOTER</DrawModal.Footer>
+        </DrawModal>
+        <DrawModal
+          {...args}
+          opened={rightOpened}
+          onClose={() => {
+            setRightOpened(false);
+          }}
+          direction="right"
         >
           <DrawModal.Header>HEADER</DrawModal.Header>
           <DrawModal.Body>BODY</DrawModal.Body>
