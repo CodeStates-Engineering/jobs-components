0cd1697ffb023298691124bd1b719edfd6f71999
genuine-seok <genuine-seok@users.noreply.github.com>
2023-06-07 01:05:56 +0000

    deploy: b57fd84c1a6acaa86f69ff1f564908d9d74159a8

--- assets/index.stories-a0ee5815.js
+++
@@ -1,7 +0,0 @@
-import{j as o}from"./jsx-runtime-94f6e698.js";import{A as t}from"./index-78ddb069.js";import"./index-64131664.js";import{D as n}from"./dummy-d4c113ac.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./useSubscribedState-9b53482c.js";import"./index-d2f8bd0f.js";import"./index-e3b5ba34.js";import"./isObject-9c705d90.js";import"./_Uint8Array-6bb687c3.js";import"./index-035e1a2c.js";import"./index-18098115.js";import"./useTypography-0e7bc425.js";import"./index-fff92b81.js";import"./index-d0a38a37.js";import"./index-1fc0ca9a.js";import"./index-5f720287.js";import"./index-4ced8def.js";import"./index-b3577b86.js";import"./index-a622c86c.js";import"./index-47d8ffac.js";import"./index-02f95d9c.js";import"./chevron-down-735b3f0e.js";const X={title:"molecules/Accordion",component:t,decorators:[i=>o.jsx("div",{style:{height:"50vh",width:"300px",padding:"20px"},children:o.jsx(i,{})})]},r={render:i=>o.jsxs(t,{...i,children:[o.jsx(t.Title,{children:"Accordion Title"}),o.jsx(t.Contents,{children:n.COMMON.TEXT_LONG})]})};var e,c,m;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  render: args => <Accordion {...args}>
-      <Accordion.Title>Accordion Title</Accordion.Title>
-      <Accordion.Contents>{DUMMY.COMMON.TEXT_LONG}</Accordion.Contents>
-    </Accordion>
-}`,...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const Y=["Default"];export{r as Default,Y as __namedExportsOrder,X as default};
-//# sourceMappingURL=index.stories-a0ee5815.js.map
