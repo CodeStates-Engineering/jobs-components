1ea52e2b765f03a002404eba4e2daa19e2cb489d
HyeokjaeLee <HyeokjaeLee@users.noreply.github.com>
2023-06-08 06:43:11 +0000

    deploy: 764fc2354f719a5e750531ff877492ad7f515fa6

---
+++ assets/index.stories-d33f2c58.js
@@ -0,0 +1,56 @@
+import{j as o}from"./jsx-runtime-94f6e698.js";import{M as t}from"./index-aa29a231.js";import"./index-64131664.js";import"./index-976bee3a.js";import{a as M,b}from"./index-11ff5a16.js";import{B as u}from"./index-18098115.js";import{D as h}from"./dummy-d4c113ac.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-42c81d68.js";import"./index-bdf16ae0.js";import"./useSubscribedState-9b53482c.js";import"./index-d2f8bd0f.js";import"./index-e3b5ba34.js";import"./isObject-9c705d90.js";import"./_Uint8Array-6bb687c3.js";import"./useValidation-c7011735.js";import"./chevron-down-735b3f0e.js";import"./index-1fc0ca9a.js";import"./index-1a5f2b4c.js";import"./index-8ce4a492.js";import"./index-985a9732.js";import"./useTypography-0e7bc425.js";import"./index-dd89b1a3.js";import"./index-e468d445.js";import"./index-e74cdf5e.js";import"./index-4a8fc19f.js";import"./index-a622c86c.js";import"./index-048f7a82.js";import"./index-4b14d235.js";import"./index-b817bc71.js";import"./index-bf8692d8.js";import"./index-47d8ffac.js";import"./index-71dbd465.js";import"./index-02f95d9c.js";import"./index-1592a661.js";import"./index-54981296.js";import"./x-59731629.js";import"./index-4d0458ec.js";import"./index-d384cd4d.js";import"./index-76af0b7f.js";import"./index-e81933e5.js";import"./index-26181893.js";import"./index-6ea5475d.js";import"./index-b3577b86.js";const po={title:"organisms/Modal",component:t,decorators:[e=>o.jsx("div",{style:{height:"100vh",width:"100%"},children:o.jsx(e,{})})]},n={render:e=>{const r=M(),{search:i}=b();return o.jsxs(o.Fragment,{children:[o.jsx(u,{onClick:()=>{r("/?tab=1")},children:"Open"}),o.jsxs(t,{...e,opened:i.includes("?tab="),onClose:()=>r("/"),children:[o.jsx(t.Header,{}),o.jsx(t.Body,{children:o.jsx("p",{children:h.COMMON.TEXT_MIDDLE})}),o.jsx(t.Footer,{children:"footer children"})]})]})}},a={render:e=>{const r=M(),{search:i}=b();return o.jsxs(o.Fragment,{children:[o.jsx(u,{onClick:()=>{r("/?tab=1")},children:"Open"}),o.jsxs(t,{...e,opened:i.includes("?tab="),onClose:()=>r("/"),children:[o.jsx(t.TabMenuHeader,{items:[{label:"Tab 1",to:"/?tab=1"},{label:"Tab 2",to:"/?tab=2"},{label:"Tab 3",to:"/?tab=3"},{label:"Tab 4",to:"/?tab=4"}]}),o.jsx(t.Body,{children:o.jsx("p",{children:h.COMMON.TEXT_MIDDLE})}),o.jsx(t.Footer,{children:"footer children"})]})]})}};var s,p,l;n.parameters={...n.parameters,docs:{...(s=n.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: args => {
+    const navigate = useNavigate();
+    const {
+      search
+    } = useLocation();
+    return <>
+        <Button onClick={() => {
+        navigate('/?tab=1');
+      }}>
+          Open
+        </Button>
+        <Modal {...args} opened={search.includes('?tab=')} onClose={() => navigate('/')}>
+          <Modal.Header />
+          <Modal.Body>
+            <p>{DUMMY.COMMON.TEXT_MIDDLE}</p>
+          </Modal.Body>
+          <Modal.Footer>footer children</Modal.Footer>
+        </Modal>
+      </>;
+  }
+}`,...(l=(p=n.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var d,m,c;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: args => {
+    const navigate = useNavigate();
+    const {
+      search
+    } = useLocation();
+    return <>
+        <Button onClick={() => {
+        navigate('/?tab=1');
+      }}>
+          Open
+        </Button>
+        <Modal {...args} opened={search.includes('?tab=')} onClose={() => navigate('/')}>
+          <Modal.TabMenuHeader items={[{
+          label: 'Tab 1',
+          to: '/?tab=1'
+        }, {
+          label: 'Tab 2',
+          to: '/?tab=2'
+        }, {
+          label: 'Tab 3',
+          to: '/?tab=3'
+        }, {
+          label: 'Tab 4',
+          to: '/?tab=4'
+        }]} />
+          <Modal.Body>
+            <p>{DUMMY.COMMON.TEXT_MIDDLE}</p>
+          </Modal.Body>
+          <Modal.Footer>footer children</Modal.Footer>
+        </Modal>
+      </>;
+  }
+}`,...(c=(m=a.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const lo=["Default","WithTabMenu"];export{n as Default,a as WithTabMenu,lo as __namedExportsOrder,po as default};
+//# sourceMappingURL=index.stories-d33f2c58.js.map
