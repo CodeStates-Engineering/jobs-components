38d0d4b3c51f70acc50aee7599b343a09ca1ab76
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-06-08 12:48:43 +0900

    fix: FocusLayer 버그 수정
    - FocusLayer가 비활성화 시에도 body에 영향을 미치는 버그 수정

--- src/components/atoms/FocusLayer/index.tsx
+++ src/components/atoms/FocusLayer/index.tsx
@@ -65,16 +65,18 @@ export const FocusLayer = ({
     createPortal(
       <>
         {focusStatus ? (
-          <div
-            onClick={onBlur}
-            className={cleanClassName(
-              `${styles['background-layer']} ${
-                styles[`priority-${priority}`]
-              } ${isClosing && styles.closing} ${blur && styles.blur}`,
-            )}
-          />
+          <>
+            <div
+              onClick={onBlur}
+              className={cleanClassName(
+                `${styles['background-layer']} ${
+                  styles[`priority-${priority}`]
+                } ${isClosing && styles.closing} ${blur && styles.blur}`,
+              )}
+            />
+            <div {...childrenWrapProps} />
+          </>
         ) : null}
-        <div {...childrenWrapProps} />
       </>,
       document.body,
     )
