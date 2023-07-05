d60e20d55ba2ce500c521ee67749e20f2536f744
genuine-seok <genuineseok@gmail.com>
2023-07-05 13:49:54 +0900

    chore: useValidate 커스텀 훅 모듈 export 수정

--- src/hooks/public/index.ts
+++ src/hooks/public/index.ts
@@ -4,3 +4,4 @@ export * from './useMountedEffect';
 export * from './useMountedLayoutEffect';
 export * from './useSubscribedState';
 export * from './useValidationStore';
+export * from "./useValidate";
