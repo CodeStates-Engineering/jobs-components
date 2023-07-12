676b7dc11142875ba844081e575173d575f3ab19
genuine-seok <genuine-seok@users.noreply.github.com>
2023-07-12 08:36:05 +0000

    deploy: 7a5f2f0358b6e63def2ed466fec06e09fb292f87

--- assets/preview-301f5c81.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-746d55ad.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-4eed915e.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-4eed915e.js","./iframe-746d55ad.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-7fe832a8.js","./index-8ce4a492.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-301f5c81.js.map
