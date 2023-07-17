381a164f69de314f687f6e955246984eb9b01c45
genuine-seok <genuine-seok@users.noreply.github.com>
2023-07-17 08:12:14 +0000

    deploy: 6ee5cde80c4f9ca990ce1dc780d4fe600cd74b98

---
+++ components/organisms/NotFound/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+export interface NotFoundProps {
+    description: string;
+    buttonTitle: string;
+    onConfirm: () => void;
+}
+export declare const NotFound: ({ description, buttonTitle, onConfirm, }: NotFoundProps) => JSX.Element;
---
+++ components/organisms/NotFound/index.stories.d.ts
@@ -0,0 +1,6 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { NotFound } from '.';
+declare const meta: Meta<typeof NotFound>;
+export default meta;
+type Story = StoryObj<typeof NotFound>;
+export declare const Default: Story;
