21b4aafc3fdc83b6b4a7a506c06ec9e9838c3041
HyeokjaeLee <HyeokjaeLee@users.noreply.github.com>
2023-07-26 08:56:31 +0000

    deploy: f8ea0fe983681e192681736b88ef69c265abc0e8

--- components/atoms/Button/index.d.ts
+++ components/atoms/Button/index.d.ts
@@ -3,11 +3,11 @@ import type { UseTypographyClassNameParams } from '../../../hooks';
 type HtmlButtonProps = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
 export interface ButtonProps extends Pick<HtmlButtonProps, 'children' | 'disabled' | 'type' | 'onClick' | 'className'>, UseTypographyClassNameParams, Pick<React.CSSProperties, 'width'> {
     delay?: number;
-    size?: 'small3x' | 'small' | 'medium' | 'large';
-    theme?: 'white/purple600' | 'white/bluish-gray800' | 'purple600/0' | 'bluish-gray700/0' | 'bluish-gray300/0' | 'bluish-gray500/0' | 'bluish-gray400/0/bluish-gray200' | 'bluish-gray700/0/bluish-gray200' | 'bluish-gray400/bluish-gray10/bluish-gray200';
+    size?: 'small3x' | 'small2x' | 'small' | 'medium' | 'large';
+    theme?: 'white/purple600' | 'white/bluish-gray800' | 'purple600/0' | 'bluish-gray700/0' | 'bluish-gray300/0' | 'bluish-gray500/0' | 'bluish-gray400/0/bluish-gray200' | 'bluish-gray700/0/bluish-gray200' | 'bluish-gray400/bluish-gray10/bluish-gray200' | 'purple550/purple50/purple100';
     icon?: ReactNode;
     iconDirection?: 'left' | 'right';
-    shape?: 'round' | 'default';
+    shape?: 'pill' | '8' | '4';
     padding?: boolean;
     focusOutline?: boolean;
     textAlign?: 'left' | 'center' | 'right';
