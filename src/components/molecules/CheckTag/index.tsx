8599452b47a81fc3f31a757e27d73ec9d9a508d7
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-07-27 15:23:47 +0900

    JOBS-761(CheckTag): 컴포넌트 개발 (#80)
    * feat(CheckTag): 컴포넌트 개발
    
    * fix: module export 추가

---
+++ src/components/molecules/CheckTag/index.tsx
@@ -0,0 +1,57 @@
+import { Check } from 'react-feather';
+
+import { Button } from '@components/atoms';
+import { useSubscribedState } from '@hooks';
+import { cleanClassName } from '@utils';
+
+import styles from './index.module.scss';
+
+export interface CheckTagProps {
+  value?: boolean;
+  onChange?: (value: boolean) => void;
+  children?: React.ReactNode;
+}
+
+export const CheckTag = ({
+  value = false,
+  onChange,
+  children,
+}: CheckTagProps) => {
+  const [checked, setChecked] = useSubscribedState(value);
+  const isForClose = false;
+
+  return (
+    <Button
+      className={styles.padding}
+      size="small2x"
+      theme={
+        checked
+          ? 'purple550/purple50/purple100'
+          : 'bluish-gray700/0/bluish-gray200'
+      }
+      iconDirection="right"
+      fontSize="small3x"
+      fontWeight={500}
+      padding={false}
+      shape="4"
+      onClick={
+        isForClose
+          ? undefined
+          : () => {
+              const newValue = !checked;
+              setChecked(newValue);
+              onChange?.(newValue);
+            }
+      }
+    >
+      <div className={styles['children-container']}>
+        {children}
+        <Check
+          className={cleanClassName(
+            `${styles['check-icon']} ${checked || styles.hidden}`,
+          )}
+        />
+      </div>
+    </Button>
+  );
+};
