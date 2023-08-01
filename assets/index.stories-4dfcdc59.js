90d6cf486fa68679c267cbfb39a75f6bdd402a71
genuine-seok <genuine-seok@users.noreply.github.com>
2023-08-01 01:56:17 +0000

    deploy: 5c0298cfdb9097b326adc4e987074491f522b59a

---
+++ assets/index.stories-4dfcdc59.js
@@ -0,0 +1,15 @@
+import{j as t}from"./jsx-runtime-94f6e698.js";import{r as c}from"./index-8db94870.js";import{T as i}from"./index-6a89d6dc.js";import"./index-230dbeff.js";import{B as d}from"./index-1e85bc29.js";import"./_commonjsHelpers-042e6b4d.js";import"./ValidationContext-e5b2e92a.js";import"./index-72bf1363.js";import"./index-23a2e887.js";import"./Compatibility-ff615e1c.js";import"./useValidate-fe71e470.js";import"./index-d92d2610.js";import"./index-1fc0ca9a.js";import"./index-9c1733cd.js";import"./index-8ce4a492.js";import"./index-284d58f2.js";import"./index-51f28030.js";import"./index-f159dff6.js";import"./index-38f6503f.js";import"./isObject-5520b7e1.js";import"./index-1116b26a.js";const w={title:"molecules/ToastContainer",component:i,decorators:[e=>t.jsx("div",{style:{height:"100vh",width:"100%",padding:"20px"},children:t.jsx(e,{})})]},o={render:e=>{const[s,p]=c.useState(0);return t.jsxs(t.Fragment,{children:[t.jsx(d,{onClick:()=>p(m=>m+1),children:"Toast Up"}),t.jsx(i,{...e,toastOption:{message:s?`Toast test text ${s}`:null,type:s%2===0?"success":"fail"}})]})}};var r,n,a;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => {
+    const [toastIndex, setToastIndex] = useState(0);
+    return <>
+        <Button onClick={() => setToastIndex(prev => prev + 1)}>
+          Toast Up
+        </Button>
+        <ToastContainer {...args} toastOption={{
+        message: toastIndex ? \`Toast test text \${toastIndex}\` : null,
+        type: toastIndex % 2 === 0 ? 'success' : 'fail'
+      }} />
+      </>;
+  }
+}`,...(a=(n=o.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const F=["Default"];export{o as Default,F as __namedExportsOrder,w as default};
+//# sourceMappingURL=index.stories-4dfcdc59.js.map
