983c086da087050574d8c02a85c4947716aadfed
june <ryu9663@naver.com>
2023-08-17 09:34:06 +0900

    feat: OptionsButton 컴포넌트 생성

---
+++ src/components/organisms/OptionsButton/index.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { OptionsButton } from '.';
+
+const meta: Meta<typeof OptionsButton> = {
+  title: 'organisms/OptionsButton',
+  component: OptionsButton,
+  args: {
+    buttonStyle: {
+      fontSize: 'small',
+      size: 'small',
+      theme: 'bluish-gray700/0',
+    },
+    buttonContent: '공개 설정하기',
+    optionStyle: { fontSize: 'small2x', fontWeight: 500, width: '100px' },
+    options: [
+      {
+        label: '공개',
+        value: '공개',
+      },
+      {
+        label: '비공개',
+        value: '비공개',
+      },
+    ],
+    textEllipsis: true,
+    float: 'top',
+  },
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          height: '600px',
+          width: '100%',
+
+          padding: '300px',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof OptionsButton>;
+
+export const Default: Story = {
+  render: (args) => (
+    <OptionsButton
+      {...args}
+      onChange={(value) => {
+        console.log(value);
+      }}
+    />
+  ),
+};
