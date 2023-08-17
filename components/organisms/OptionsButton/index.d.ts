33397049ab64a5c24a9f64624590c72118f2c6b0
genuine-seok <genuine-seok@users.noreply.github.com>
2023-08-17 07:45:53 +0000

    deploy: 7175e1ccfae260c52ee64c4e5b0487e728606d23

--- components/organisms/OptionsButton/index.d.ts
+++ components/organisms/OptionsButton/index.d.ts
@@ -1,10 +1,6 @@
-import type { ButtonProps, OptionsProps } from '../../atoms';
-export interface OptionsButtonProps extends Pick<OptionsProps<string, false>, 'float' | 'optionStyle' | 'onChange' | 'value' | 'textEllipsis' | 'className'> {
-    options?: {
-        label: string;
-        value: string;
-    }[];
+import type { ButtonProps, OptionsProps, ValidOptionValue } from '../../atoms';
+export interface OptionsButtonProps<_ValidOptionValue = ValidOptionValue, _Multiple = boolean> extends Pick<OptionsProps<_ValidOptionValue, _Multiple>, 'float' | 'optionStyle' | 'onChange' | 'multiple' | 'value' | 'textEllipsis' | 'className' | 'options'> {
     buttonStyle: ButtonProps;
     buttonContent: string;
 }
-export declare const OptionsButton: ({ value, onChange, options, optionStyle, float, textEllipsis, className, buttonStyle, buttonContent, }: OptionsButtonProps) => import("react/jsx-runtime").JSX.Element;
+export declare const OptionsButton: ({ value, onChange, options, optionStyle, float, textEllipsis, className, buttonStyle, buttonContent, multiple, }: OptionsButtonProps) => import("react/jsx-runtime").JSX.Element;
