0c37f814100bdd90be77b744a5d3c8c760c7632b
juchanhwang <ghkdwncks96@gmail.com>
2024-01-11 16:33:22 +0900

    feat(type/typo): 타이포그래피에 대한 스타일 정의

--- src/hooks/private/useTypographyClassName.tsx
+++ src/hooks/private/useTypographyClassName.tsx
@@ -1,19 +1,7 @@
+import type { FontSize, FontWeight } from '@type/typo';
 import { cleanClassName } from '@utils';
 
 import styles from '../../styles/typography.module.scss';
-
-type FontSize =
-  | 'large4x'
-  | 'large3x'
-  | 'large2x'
-  | 'large'
-  | 'medium'
-  | 'normal'
-  | 'small'
-  | 'small2x'
-  | 'small3x';
-
-type FontWeight = 700 | 600 | 500 | 400;
 
 export interface UseTypographyClassNameParams {
   fontSize?: FontSize;
