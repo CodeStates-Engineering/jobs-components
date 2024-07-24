45f9359bc483a35649c544dcca2774160c1606ea
juchanhwang <juchanhwang@users.noreply.github.com>
2024-07-24 06:24:26 +0000

    deploy: ddd7a1b5fbdde69d6b9c3722485983c08b1b3423

---
+++ assets/index.stories-99474234.js
@@ -0,0 +1,108 @@
+import{j as s}from"./jsx-runtime-4ca860c5.js";import{r as Ne}from"./index-61bf1805.js";import{B as u}from"./index-8ad70b66.js";import{A as Ue}from"./alert-octagon-9b6ca96b.js";import{i as h,d as Re,e as Ke,f as be,h as ze,j as Ye,M as xe,k as We,l as Te,o as qe,m as ve,n as je,s as He,p as Xe,U as R,a as P,q as A,r as Se,b as Ze,S as Ve,g as Je}from"./_Uint8Array-c13c238b.js";import{S as y,g as G,i as B,f as C,h as Qe,r as ke}from"./useValidate-983984d3.js";import"./_commonjsHelpers-de833af9.js";import"./Compatibility-d1e3055b.js";import"./cleanClassName-cdc1ff81.js";import"./index-8d47fad6.js";function Oe(e,r){for(var n=-1,t=e==null?0:e.length,o=Array(t);++n<t;)o[n]=r(e[n],n,e);return o}var er=1/0,K=y?y.prototype:void 0,z=K?K.toString:void 0;function Ae(e){if(typeof e=="string")return e;if(h(e))return Oe(e,Ae)+"";if(G(e))return z?z.call(e):"";var r=e+"";return r=="0"&&1/e==-er?"-0":r}function rr(e){return e}var Y=Object.create,nr=function(){function e(){}return function(r){if(!B(r))return{};if(Y)return Y(r);e.prototype=r;var n=new e;return e.prototype=void 0,n}}();const tr=nr;function or(e,r,n){switch(n.length){case 0:return e.call(r);case 1:return e.call(r,n[0]);case 2:return e.call(r,n[0],n[1]);case 3:return e.call(r,n[0],n[1],n[2])}return e.apply(r,n)}function ar(e,r){var n=-1,t=e.length;for(r||(r=Array(t));++n<t;)r[n]=e[n];return r}var ir=800,sr=16,lr=Date.now;function cr(e){var r=0,n=0;return function(){var t=lr(),o=sr-(t-n);if(n=t,o>0){if(++r>=ir)return arguments[0]}else r=0;return e.apply(void 0,arguments)}}function ur(e){return function(){return e}}var fr=function(){try{var e=Re(Object,"defineProperty");return e({},"",{}),e}catch{}}();const $=fr;var pr=$?function(e,r){return $(e,"toString",{configurable:!0,enumerable:!1,value:ur(r),writable:!0})}:rr;const dr=pr;var gr=cr(dr);const yr=gr;function hr(e,r){for(var n=-1,t=e==null?0:e.length;++n<t&&r(e[n],n,e)!==!1;);return e}function $e(e,r,n){r=="__proto__"&&$?$(e,r,{configurable:!0,enumerable:!0,value:n,writable:!0}):e[r]=n}var mr=Object.prototype,br=mr.hasOwnProperty;function we(e,r,n){var t=e[r];(!(br.call(e,r)&&Ke(t,n))||n===void 0&&!(r in e))&&$e(e,r,n)}function m(e,r,n,t){var o=!n;n||(n={});for(var a=-1,l=r.length;++a<l;){var c=r[a],f=t?t(n[c],e[c],c,n,e):void 0;f===void 0&&(f=e[c]),o?$e(n,c,f):we(n,c,f)}return n}var W=Math.max;function xr(e,r,n){return r=W(r===void 0?e.length-1:r,0),function(){for(var t=arguments,o=-1,a=W(t.length-r,0),l=Array(a);++o<a;)l[o]=t[r+o];o=-1;for(var c=Array(r+1);++o<r;)c[o]=t[o];return c[r]=n(l),or(e,this,c)}}function Tr(e){var r=[];if(e!=null)for(var n in Object(e))r.push(n);return r}var vr=Object.prototype,jr=vr.hasOwnProperty;function Sr(e){if(!B(e))return Tr(e);var r=be(e),n=[];for(var t in e)t=="constructor"&&(r||!jr.call(e,t))||n.push(t);return n}function I(e){return ze(e)?Ye(e,!0):Sr(e)}var Or=/\.|\[(?:[^[\]]*|(["'])(?:(?!\1)[^\\]|\\.)*?\1)\]/,Ar=/^\w*$/;function $r(e,r){if(h(e))return!1;var n=typeof e;return n=="number"||n=="symbol"||n=="boolean"||e==null||G(e)?!0:Ar.test(e)||!Or.test(e)||r!=null&&e in Object(r)}var wr="Expected a function";function E(e,r){if(typeof e!="function"||r!=null&&typeof r!="function")throw new TypeError(wr);var n=function(){var t=arguments,o=r?r.apply(this,t):t[0],a=n.cache;if(a.has(o))return a.get(o);var l=e.apply(this,t);return n.cache=a.set(o,l)||a,l};return n.cache=new(E.Cache||xe),n}E.Cache=xe;var Pr=500;function Gr(e){var r=E(e,function(t){return n.size===Pr&&n.clear(),t}),n=r.cache;return r}var Br=/[^.[\]]+|\[(?:(-?\d+(?:\.\d+)?)|(["'])((?:(?!\2)[^\\]|\\.)*?)\2)\]|(?=(?:\.|\[\])(?:\.|\[\]|$))/g,Cr=/\\(\\)?/g,Ir=Gr(function(e){var r=[];return e.charCodeAt(0)===46&&r.push(""),e.replace(Br,function(n,t,o,a){r.push(o?a.replace(Cr,"$1"):t||n)}),r});const Er=Ir;function Fr(e){return e==null?"":Ae(e)}function F(e,r){return h(e)?e:$r(e,r)?[e]:Er(Fr(e))}var Dr=1/0;function Pe(e){if(typeof e=="string"||G(e))return e;var r=e+"";return r=="0"&&1/e==-Dr?"-0":r}function _r(e,r){r=F(r,e);for(var n=0,t=r.length;e!=null&&n<t;)e=e[Pe(r[n++])];return n&&n==t?e:void 0}var q=y?y.isConcatSpreadable:void 0;function Lr(e){return h(e)||We(e)||!!(q&&e&&e[q])}function Ge(e,r,n,t,o){var a=-1,l=e.length;for(n||(n=Lr),o||(o=[]);++a<l;){var c=e[a];r>0&&n(c)?r>1?Ge(c,r-1,n,t,o):Te(o,c):t||(o[o.length]=c)}return o}function Mr(e){var r=e==null?0:e.length;return r?Ge(e,1):[]}function Nr(e){return yr(xr(e,void 0,Mr),e+"")}var Ur=qe(Object.getPrototypeOf,Object);const D=Ur;var Rr="[object Object]",Kr=Function.prototype,zr=Object.prototype,Be=Kr.toString,Yr=zr.hasOwnProperty,Wr=Be.call(Object);function qr(e){if(!C(e)||Qe(e)!=Rr)return!1;var r=D(e);if(r===null)return!0;var n=Yr.call(r,"constructor")&&r.constructor;return typeof n=="function"&&n instanceof n&&Be.call(n)==Wr}function Hr(e,r,n){var t=-1,o=e.length;r<0&&(r=-r>o?0:o+r),n=n>o?o:n,n<0&&(n+=o),o=r>n?0:n-r>>>0,r>>>=0;for(var a=Array(o);++t<o;)a[t]=e[t+r];return a}function Xr(e,r){return e&&m(r,ve(r),e)}function Zr(e,r){return e&&m(r,I(r),e)}var Ce=typeof exports=="object"&&exports&&!exports.nodeType&&exports,H=Ce&&typeof module=="object"&&module&&!module.nodeType&&module,Vr=H&&H.exports===Ce,X=Vr?ke.Buffer:void 0,Z=X?X.allocUnsafe:void 0;function Jr(e,r){if(r)return e.slice();var n=e.length,t=Z?Z(n):new e.constructor(n);return e.copy(t),t}function Qr(e,r){return m(e,je(e),r)}var kr=Object.getOwnPropertySymbols,en=kr?function(e){for(var r=[];e;)Te(r,je(e)),e=D(e);return r}:He;const Ie=en;function rn(e,r){return m(e,Ie(e),r)}function Ee(e){return Xe(e,I,Ie)}var nn=Object.prototype,tn=nn.hasOwnProperty;function on(e){var r=e.length,n=new e.constructor(r);return r&&typeof e[0]=="string"&&tn.call(e,"index")&&(n.index=e.index,n.input=e.input),n}function _(e){var r=new e.constructor(e.byteLength);return new R(r).set(new R(e)),r}function an(e,r){var n=r?_(e.buffer):e.buffer;return new e.constructor(n,e.byteOffset,e.byteLength)}var sn=/\w*$/;function ln(e){var r=new e.constructor(e.source,sn.exec(e));return r.lastIndex=e.lastIndex,r}var V=y?y.prototype:void 0,J=V?V.valueOf:void 0;function cn(e){return J?Object(J.call(e)):{}}function un(e,r){var n=r?_(e.buffer):e.buffer;return new e.constructor(n,e.byteOffset,e.length)}var fn="[object Boolean]",pn="[object Date]",dn="[object Map]",gn="[object Number]",yn="[object RegExp]",hn="[object Set]",mn="[object String]",bn="[object Symbol]",xn="[object ArrayBuffer]",Tn="[object DataView]",vn="[object Float32Array]",jn="[object Float64Array]",Sn="[object Int8Array]",On="[object Int16Array]",An="[object Int32Array]",$n="[object Uint8Array]",wn="[object Uint8ClampedArray]",Pn="[object Uint16Array]",Gn="[object Uint32Array]";function Bn(e,r,n){var t=e.constructor;switch(r){case xn:return _(e);case fn:case pn:return new t(+e);case Tn:return an(e,n);case vn:case jn:case Sn:case On:case An:case $n:case wn:case Pn:case Gn:return un(e,n);case dn:return new t;case gn:case mn:return new t(e);case yn:return ln(e);case hn:return new t;case bn:return cn(e)}}function Cn(e){return typeof e.constructor=="function"&&!be(e)?tr(D(e)):{}}var In="[object Map]";function En(e){return C(e)&&P(e)==In}var Q=A&&A.isMap,Fn=Q?Se(Q):En;const Dn=Fn;var _n="[object Set]";function Ln(e){return C(e)&&P(e)==_n}var k=A&&A.isSet,Mn=k?Se(k):Ln;const Nn=Mn;var Un=1,Rn=2,Kn=4,Fe="[object Arguments]",zn="[object Array]",Yn="[object Boolean]",Wn="[object Date]",qn="[object Error]",De="[object Function]",Hn="[object GeneratorFunction]",Xn="[object Map]",Zn="[object Number]",_e="[object Object]",Vn="[object RegExp]",Jn="[object Set]",Qn="[object String]",kn="[object Symbol]",et="[object WeakMap]",rt="[object ArrayBuffer]",nt="[object DataView]",tt="[object Float32Array]",ot="[object Float64Array]",at="[object Int8Array]",it="[object Int16Array]",st="[object Int32Array]",lt="[object Uint8Array]",ct="[object Uint8ClampedArray]",ut="[object Uint16Array]",ft="[object Uint32Array]",i={};i[Fe]=i[zn]=i[rt]=i[nt]=i[Yn]=i[Wn]=i[tt]=i[ot]=i[at]=i[it]=i[st]=i[Xn]=i[Zn]=i[_e]=i[Vn]=i[Jn]=i[Qn]=i[kn]=i[lt]=i[ct]=i[ut]=i[ft]=!0;i[qn]=i[De]=i[et]=!1;function O(e,r,n,t,o,a){var l,c=r&Un,f=r&Rn,Le=r&Kn;if(n&&(l=o?n(e,t,o,a):n(e)),l!==void 0)return l;if(!B(e))return e;var L=h(e);if(L){if(l=on(e),!c)return ar(e,l)}else{var g=P(e),M=g==De||g==Hn;if(Ze(e))return Jr(e,c);if(g==_e||g==Fe||M&&!o){if(l=f||M?{}:Cn(e),!c)return f?rn(e,Zr(l,e)):Qr(e,Xr(l,e))}else{if(!i[g])return o?e:{};l=Bn(e,g,c)}}a||(a=new Ve);var N=a.get(e);if(N)return N;a.set(e,l),Nn(e)?e.forEach(function(p){l.add(O(p,r,n,p,e,a))}):Dn(e)&&e.forEach(function(p,d){l.set(d,O(p,r,n,d,e,a))});var Me=Le?f?Ee:Je:f?I:ve,U=L?void 0:Me(e);return hr(U||e,function(p,d){U&&(d=p,p=e[d]),we(l,d,O(p,r,n,d,e,a))}),l}function pt(e){var r=e==null?0:e.length;return r?e[r-1]:void 0}function dt(e,r){return r.length<2?e:_r(e,Hr(r,0,-1))}function gt(e,r){return r=F(r,e),e=dt(e,r),e==null||delete e[Pe(pt(r))]}function yt(e){return qr(e)?void 0:e}var ht=1,mt=2,bt=4,xt=Nr(function(e,r){var n={};if(e==null)return n;var t=!1;r=Oe(r,function(a){return a=F(a,e),t||(t=a.length>1),a}),m(e,Ee(e),n),t&&(n=O(n,ht|mt|bt,yt));for(var o=r.length;o--;)gt(n,r[o]);return n});const Tt=xt,w={contained:["purple550","purple600","bluishGray600","bluishGray700","bluishGray800"],ghost:["purple600","bluishGray300","bluishGray400","bluishGray500","bluishGray600","bluishGray700","bluishGray800"],outlined:["bluishGray400","bluishGray500","bluishGray600","bluishGray700","bluishGray800"]},Ct={title:"atoms/Button",component:u,argTypes:{disabled:{control:{type:"boolean",default:!1}},children:{control:{type:"text"}},delay:{control:{type:"number"},description:"버튼은 클릭 전 딜레이 시간을 가질 수 있다."},variant:{control:{type:"radio",options:Object.keys(w)}},color:{control:{type:"select",options:e=>w[e.variant??"contained"]||[]}}},args:{variant:"contained",color:"purple600",children:"Button",disabled:!1,padding:!0,size:"large",fontSize:"normal",fontWeight:700,iconDirection:"left",shape:"8"},decorators:[(e,r)=>{const{variant:n}=r.args;return r.argTypes.color&&(r.argTypes.color.options=w[n??"contained"]),s.jsx("article",{style:{display:"flex",gap:"10px",flexWrap:"wrap",alignItems:"flex-end"},children:s.jsx(e,{})})}]},b={},x={render:e=>s.jsxs("div",{style:{display:"flex",gap:"80px"},children:[s.jsxs("div",{style:{display:"flex",gap:"16px",flexDirection:"column"},children:[s.jsx("h3",{children:"Contained"}),["purple550","purple600","bluishGray400","bluishGray800"].map((r,n)=>s.jsx(u,{...e,color:r,key:n,variant:"contained",children:r}))]}),s.jsxs("div",{style:{display:"flex",gap:"16px",flexDirection:"column"},children:[s.jsx("h3",{children:"Ghost"}),["purple600","bluishGray300","bluishGray700"].map((r,n)=>s.jsx(u,{...e,color:r,key:n,variant:"ghost",children:r}))]}),s.jsxs("div",{style:{display:"flex",gap:"16px",flexDirection:"column"},children:[s.jsx("h3",{children:"Outlined"}),["bluishGray400","bluishGray700"].map((r,n)=>s.jsx(u,{...e,color:r,key:n,variant:"outlined",children:r}))]})]})},T={render:e=>s.jsx(s.Fragment,{children:["pill","8","4"].map(r=>Ne.createElement(u,{...e,shape:r,key:r},r))})},v={render:e=>s.jsx(s.Fragment,{children:["small","medium","large"].map((r,n)=>s.jsx(u,{...e,size:r,key:n,children:r}))})},j={args:{icon:s.jsx(Ue,{})},render:e=>s.jsx("div",{style:{display:"flex",gap:"10px",flexDirection:"column"},children:["pill","8","4"].map(r=>s.jsxs("div",{style:{display:"flex",gap:"10px"},children:[s.jsx(u,{...e,shape:r}),s.jsx(u,{...Tt({...e,shape:r},"children")}),s.jsx(u,{...e,shape:r,iconDirection:"right"})]},r))})},S={args:{delay:1e4},render:e=>s.jsx(s.Fragment,{children:["purple600"].map((r,n)=>s.jsx(u,{...e,color:r,key:n}))})};var ee,re,ne;b.parameters={...b.parameters,docs:{...(ee=b.parameters)==null?void 0:ee.docs,source:{originalSource:"{}",...(ne=(re=b.parameters)==null?void 0:re.docs)==null?void 0:ne.source}}};var te,oe,ae;x.parameters={...x.parameters,docs:{...(te=x.parameters)==null?void 0:te.docs,source:{originalSource:`{
+  render: args => <div style={{
+    display: 'flex',
+    gap: '80px'
+  }}>
+      <div style={{
+      display: 'flex',
+      gap: '16px',
+      flexDirection: 'column'
+    }}>
+        <h3>Contained</h3>
+        {(['purple550', 'purple600', 'bluishGray400', 'bluishGray800'] satisfies ButtonProps['color'][]).map((color, key) => <Button {...{
+        ...args,
+        color,
+        key,
+        variant: 'contained'
+      }}>
+            {color}
+          </Button>)}
+      </div>
+      <div style={{
+      display: 'flex',
+      gap: '16px',
+      flexDirection: 'column'
+    }}>
+        <h3>Ghost</h3>
+        {(['purple600', 'bluishGray300', 'bluishGray700'] satisfies ButtonProps['color'][]).map((color, key) => <Button {...{
+        ...args,
+        color,
+        key,
+        variant: 'ghost'
+      }}>
+            {color}
+          </Button>)}
+      </div>
+      <div style={{
+      display: 'flex',
+      gap: '16px',
+      flexDirection: 'column'
+    }}>
+        <h3>Outlined</h3>
+        {(['bluishGray400', 'bluishGray700'] satisfies ButtonProps['color'][]).map((color, key) => <Button {...{
+        ...args,
+        color,
+        key,
+        variant: 'outlined'
+      }}>
+            {color}
+          </Button>)}
+      </div>
+    </div>
+}`,...(ae=(oe=x.parameters)==null?void 0:oe.docs)==null?void 0:ae.source}}};var ie,se,le;T.parameters={...T.parameters,docs:{...(ie=T.parameters)==null?void 0:ie.docs,source:{originalSource:`{
+  render: args => <>
+      {(['pill', '8', '4'] satisfies ButtonProps['shape'][]).map(shape => <Button {...{
+      ...args,
+      shape
+    }} key={shape}>
+          {shape}
+        </Button>)}
+    </>
+}`,...(le=(se=T.parameters)==null?void 0:se.docs)==null?void 0:le.source}}};var ce,ue,fe;v.parameters={...v.parameters,docs:{...(ce=v.parameters)==null?void 0:ce.docs,source:{originalSource:`{
+  render: args => <>
+      {(['small', 'medium', 'large'] satisfies ButtonProps['size'][]).map((size, key) => <Button {...{
+      ...args,
+      size,
+      key
+    }}>{size}</Button>)}
+    </>
+}`,...(fe=(ue=v.parameters)==null?void 0:ue.docs)==null?void 0:fe.source}}};var pe,de,ge;j.parameters={...j.parameters,docs:{...(pe=j.parameters)==null?void 0:pe.docs,source:{originalSource:`{
+  args: {
+    icon: <AlertOctagon />
+  },
+  render: args => <div style={{
+    display: 'flex',
+    gap: '10px',
+    flexDirection: 'column'
+  }}>
+      {(['pill', '8', '4'] satisfies ButtonProps['shape'][]).map(shape => <div style={{
+      display: 'flex',
+      gap: '10px'
+    }} key={shape}>
+          <Button {...{
+        ...args,
+        shape
+      }} />
+          <Button {...omit({
+        ...args,
+        shape
+      }, 'children')} />
+          <Button {...{
+        ...args,
+        shape
+      }} iconDirection="right" />
+        </div>)}
+    </div>
+}`,...(ge=(de=j.parameters)==null?void 0:de.docs)==null?void 0:ge.source}}};var ye,he,me;S.parameters={...S.parameters,docs:{...(ye=S.parameters)==null?void 0:ye.docs,source:{originalSource:`{
+  args: {
+    delay: 10000
+  },
+  render: args => <>
+      {(['purple600'] satisfies ButtonProps['color'][]).map((color, key) => <Button {...{
+      ...args,
+      color,
+      key
+    }} />)}
+    </>
+}`,...(me=(he=S.parameters)==null?void 0:he.docs)==null?void 0:me.source}}};const It=["Default","VariantAndColors","Shape","Size","Icon","Delay"];export{b as Default,S as Delay,j as Icon,T as Shape,v as Size,x as VariantAndColors,It as __namedExportsOrder,Ct as default};
+//# sourceMappingURL=index.stories-99474234.js.map
