72d1fd8b0e62bbbc5396736f44172b0465c2f42d
juchanhwang <juchanhwang@users.noreply.github.com>
2024-08-27 08:48:28 +0000

    deploy: c0d15d55f5687e69a019ff26ac5ed18429d496f1

---
+++ assets/index.stories-2ab091d0.js
@@ -0,0 +1,43 @@
+import{j as t}from"./jsx-runtime-4ca860c5.js";import{r as f}from"./index-61bf1805.js";import"./index-775a2672.js";import"./index-b22df04f.js";import{F as l}from"./index-f390a560.js";import{X as u}from"./x-06d309c3.js";import{B as c}from"./index-12a8a271.js";import"./_commonjsHelpers-de833af9.js";import"./index-1715ee98.js";import"./Compatibility-d1e3055b.js";import"./useValidate-983984d3.js";import"./cleanClassName-cdc1ff81.js";import"./index-868581da.js";import"./index-8d47fad6.js";import"./index-be44e53b.js";import"./index-2801d3c9.js";import"./index-43870500.js";import"./index-23c7aa51.js";import"./index-576e98e6.js";import"./index-5240e4a2.js";import"./index-cde9edb5.js";import"./index-e6fc3cfe.js";import"./index-88ef95b6.js";import"./useSubscribedState-9e613bb6.js";import"./_Uint8Array-c13c238b.js";import"./chevron-down-2f1d20ed.js";import"./index-9cf50c71.js";import"./index-2b7a968e.js";import"./index-fea155a7.js";import"./index-7114117d.js";import"./index-1f827378.js";import"./index-b478107a.js";import"./index-abdfd4fb.js";import"./index-8cbdbd53.js";import"./index-d80b4b01.js";import"./index-03500a04.js";import"./index-234297c6.js";import"./DUMMY-096d32b1.js";import"./index-0c72020f.js";import"./index-2c208425.js";import"./index-f6955148.js";import"./index-628c941e.js";import"./index-cc2f50dc.js";const i={"action-floating-modal-container":"_action-floating-modal-container_extnz_461","action-count":"_action-count_extnz_467","action-count-circle":"_action-count-circle_extnz_475","action-count-text":"_action-count-text_extnz_487","fade-in":"_fade-in_extnz_1","fade-out":"_fade-out_extnz_1","shake-horizontal":"_shake-horizontal_extnz_1","tilt-in-fwd-tr":"_tilt-in-fwd-tr_extnz_1","slide-in-fwd-center":"_slide-in-fwd-center_extnz_1","scale-in-center":"_scale-in-center_extnz_1","slide-in-right":"_slide-in-right_extnz_1","slide-in-left":"_slide-in-left_extnz_1","slide-out-right":"_slide-out-right_extnz_1","slide-out-left":"_slide-out-left_extnz_1","scale-in-top":"_scale-in-top_extnz_1","scale-out-top":"_scale-out-top_extnz_1","swing-in-top-fwd":"_swing-in-top-fwd_extnz_1","fade-in-top":"_fade-in-top_extnz_1","fade-out-top":"_fade-out-top_extnz_1","fade-in-bottom":"_fade-in-bottom_extnz_1","fade-out-bottom":"_fade-out-bottom_extnz_1","scale-out-horizontal":"_scale-out-horizontal_extnz_1"},a=({count:e=0,opened:r,onClose:n,children:m,className:_,priority:x=3})=>t.jsxs(l,{opened:r,className:_,priority:x,children:[t.jsx(l.Body,{children:t.jsxs("div",{className:i["action-floating-modal-container"],children:[t.jsxs("div",{className:i["action-count"],children:[t.jsx("span",{className:i["action-count-circle"],children:e})," ",t.jsx("span",{className:i["action-count-text"],children:"선택됨"})]}),m]})}),t.jsx(l.Tail,{children:t.jsx("div",{style:{display:"flex",alignSelf:"center"},children:t.jsx(c,{icon:t.jsx(u,{}),variant:"ghost",color:"bluishGray700",size:"small3x",onClick:n})})})]});try{a.displayName="FloatingActionModal",a.__docgenInfo={description:"",displayName:"FloatingActionModal",props:{count:{defaultValue:{value:"0"},description:"",name:"count",required:!1,type:{name:"number"}},opened:{defaultValue:null,description:"",name:"opened",required:!1,type:{name:"boolean"}},onClose:{defaultValue:null,description:"",name:"onClose",required:!1,type:{name:"(() => void)"}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},priority:{defaultValue:{value:"3"},description:"",name:"priority",required:!1,type:{name:"number"}}}}}catch{}const rt={title:"organisms/FloatingActionModal",component:a,decorators:[e=>t.jsx("div",{style:{height:"100vh",width:"100%"},children:t.jsx(e,{})})]},o={render:e=>{const[r,n]=f.useState(!1);return t.jsxs(t.Fragment,{children:[t.jsx(c,{onClick:()=>{n(!0)},children:"open floating modal"}),t.jsx(a,{...e,opened:r,onClose:()=>n(!1),children:t.jsxs("div",{style:{display:"flex",flexDirection:"row",gap:"16px",padding:"0 24px"},children:[t.jsx("div",{style:{padding:"3px 0",height:"10px",fontSize:"14px",fontWeight:"600",display:"flex",alignItems:"center"},children:"리스트2"}),t.jsx("div",{style:{padding:"3px 0",height:"10px",fontSize:"14px",fontWeight:"600",display:"flex",alignItems:"center"},children:"리스트3"})]})})]})}};var s,d,p;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: args => {
+    const [opened, setOpened] = useState(false);
+    return <>
+        <Button onClick={() => {
+        setOpened(true);
+      }}>
+          open floating modal
+        </Button>
+
+        <FloatingActionModal {...args} opened={opened} onClose={() => setOpened(false)}>
+          <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '16px',
+          padding: '0 24px'
+        }}>
+            <div style={{
+            padding: '3px 0',
+            height: '10px',
+            fontSize: '14px',
+            fontWeight: '600',
+            display: 'flex',
+            alignItems: 'center'
+          }}>
+              리스트2
+            </div>
+            <div style={{
+            padding: '3px 0',
+            height: '10px',
+            fontSize: '14px',
+            fontWeight: '600',
+            display: 'flex',
+            alignItems: 'center'
+          }}>
+              리스트3
+            </div>
+          </div>
+        </FloatingActionModal>
+      </>;
+  }
+}`,...(p=(d=o.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};const lt=["Default"];export{o as Default,lt as __namedExportsOrder,rt as default};
+//# sourceMappingURL=index.stories-2ab091d0.js.map
---
+++ assets/index.stories-9b090f6a.js
@@ -0,0 +1,12 @@
+import{j as o}from"./jsx-runtime-4ca860c5.js";import{T as n}from"./index-2b7a968e.js";import"./index-61bf1805.js";import"./_commonjsHelpers-de833af9.js";import"./Compatibility-d1e3055b.js";import"./index-775a2672.js";import"./index-12a8a271.js";import"./useValidate-983984d3.js";import"./cleanClassName-cdc1ff81.js";import"./index-1715ee98.js";import"./index-868581da.js";import"./index-8d47fad6.js";import"./index-be44e53b.js";import"./index-2801d3c9.js";import"./index-43870500.js";import"./index-23c7aa51.js";import"./index-576e98e6.js";import"./index-5240e4a2.js";import"./index-cde9edb5.js";import"./index-e6fc3cfe.js";const d=[{label:"전체",to:"/"},{label:"소프트웨어 개발",to:"/software-development",disabled:!0},{label:"데이터",to:"/data"},{label:"마케팅",to:"/marketing"},{label:"블록체인",to:"/?type=blockchain"},{label:"프로덕트매니저",to:"/product-manager?category=product-manager&category2=product-designer2"},{label:"데브옵스",to:"/devops"}],G={title:"molecules/TabMenu",component:n,args:{items:d,itemSize:"medium",bottomLineWeight:"medium",selectedLineWeight:"medium",fontWeight:500,fontSize:"medium"}},e={},t={render:p=>o.jsx(o.Fragment,{children:o.jsx(n,{...p,selectedLineWeight:"none",bottomLineWeight:"none",itemSize:"small",fontSize:"small2x",variant:{default:"ghost",selected:"contained"},color:{default:"bluishGray300",selected:"purple600"},itemShape:"8"})})};var r,a,i;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var m,s,l;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: args => <>
+      <TabMenu {...args} selectedLineWeight="none" bottomLineWeight="none" itemSize="small" fontSize="small2x" variant={{
+      default: 'ghost',
+      selected: 'contained'
+    }} color={{
+      default: 'bluishGray300',
+      selected: 'purple600'
+    }} itemShape="8" />
+    </>
+}`,...(l=(s=t.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};const _=["Default","Round"];export{e as Default,t as Round,_ as __namedExportsOrder,G as default};
+//# sourceMappingURL=index.stories-9b090f6a.js.map
