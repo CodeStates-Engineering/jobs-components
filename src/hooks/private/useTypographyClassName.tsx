10601e46281c398aa4822456fa3b26cc70103f62
JuChan <ghkdwncks96@gmail.com>
2024-10-14 14:49:39 +0900

    [DTL-547] Button, Tab 컴포넌트 색상 추가 (#98)
    * feat(Button): ghost variant에 peach 컬러 추가
    
    * feat(Tag): purple600 컬러 추가

--- src/hooks/private/useTypographyClassName.tsx
+++ src/hooks/private/useTypographyClassName.tsx
@@ -1,11 +1,20 @@
-import type { FontSize, FontWeight } from '@type/typo';
 import { cleanClassName } from '@utils';
 
 import styles from '../../styles/typography.module.scss';
 
 export interface UseTypographyClassNameParams {
-  fontSize?: FontSize;
-  fontWeight?: FontWeight;
+  fontSize?:
+    | 'large4x'
+    | 'large3x'
+    | 'large2x'
+    | 'large'
+    | 'medium'
+    | 'normal'
+    | 'small'
+    | 'small2x'
+    | 'small3x'
+    | 'small4x';
+  fontWeight?: 700 | 600 | 500 | 400;
 }
 
 export const useTypographyClassName = ({
