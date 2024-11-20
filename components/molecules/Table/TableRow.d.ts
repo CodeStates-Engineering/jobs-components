711691e3dbf1854c04fdfe492139baed06d99552
juchanhwang <juchanhwang@users.noreply.github.com>
2024-11-20 06:00:25 +0000

    deploy: 3f25ec7c28cf3d9b6b47fb510f8b5d91137e193b

--- components/molecules/Table/TableRow.d.ts
+++ components/molecules/Table/TableRow.d.ts
@@ -2,5 +2,6 @@
 export interface TableRowProps {
     className?: string;
     children?: React.ReactNode;
+    ref?: (node: HTMLElement | null) => void;
 }
-export declare const TableRow: ({ children, className }: TableRowProps) => import("react/jsx-runtime").JSX.Element;
+export declare const TableRow: ({ children, className, ref }: TableRowProps) => import("react/jsx-runtime").JSX.Element;
