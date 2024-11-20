438ecaac2850751f81fb42e2f291b54c4f882897
JuChan <ghkdwncks96@gmail.com>
2024-11-20 15:10:02 +0900

    refactor(TableRow): forwardRef를 사용하여 TableRow 컴포넌트를 래핑 (#101)
    * refactor(TableRow): forwardRef를 사용하여 컴포넌트를 래핑
    
    * refactor(TableRow): export 방식 복원

--- src/components/molecules/Table/TableRow.tsx
+++ src/components/molecules/Table/TableRow.tsx
@@ -1,4 +1,4 @@
-import { Children } from 'react';
+import { Children, forwardRef } from 'react';
 
 import { useTableData } from '@contexts/TableContext';
 import { cleanClassName } from '@utils';
@@ -8,19 +8,27 @@ import styles from './TableRow.module.scss';
 export interface TableRowProps {
   className?: string;
   children?: React.ReactNode;
-  ref?: (node: HTMLElement | null) => void;
 }
 
-export const TableRow = ({ children, className, ref }: TableRowProps) => {
-  const {
-    columnDataListState: [columnDataList],
-  } = useTableData();
+export const TableRow = forwardRef<HTMLTableRowElement, TableRowProps>(
+  ({ children, className }, ref) => {
+    const {
+      columnDataListState: [columnDataList],
+    } = useTableData();
 
-  const childrenArray = Children.toArray(children);
+    const childrenArray = Children.toArray(children);
 
-  return (
-    <tr className={cleanClassName(`${styles.row} ${className}`)} ref={ref}>
-      {columnDataList.map(({ originalIndex }) => childrenArray[originalIndex])}
-    </tr>
-  );
-};
+    return (
+      <tr
+        className={cleanClassName(`${styles.row} ${className || ''}`)}
+        ref={ref}
+      >
+        {columnDataList.map(
+          ({ originalIndex }) => childrenArray[originalIndex],
+        )}
+      </tr>
+    );
+  },
+);
+
+TableRow.displayName = 'TableRow';
