3f25ec7c28cf3d9b6b47fb510f8b5d91137e193b
Gyuhyeon Jeon <ghjeon@kaist.ac.kr>
2024-11-20 14:57:00 +0900

    refactor: Table.Row 에 ref 전달 가능하도록 개선 (#100)

--- src/components/molecules/Table/TableRow.tsx
+++ src/components/molecules/Table/TableRow.tsx
@@ -8,9 +8,10 @@ import styles from './TableRow.module.scss';
 export interface TableRowProps {
   className?: string;
   children?: React.ReactNode;
+  ref?: (node: HTMLElement | null) => void;
 }
 
-export const TableRow = ({ children, className }: TableRowProps) => {
+export const TableRow = ({ children, className, ref }: TableRowProps) => {
   const {
     columnDataListState: [columnDataList],
   } = useTableData();
@@ -18,7 +19,7 @@ export const TableRow = ({ children, className }: TableRowProps) => {
   const childrenArray = Children.toArray(children);
 
   return (
-    <tr className={cleanClassName(`${styles.row} ${className}`)}>
+    <tr className={cleanClassName(`${styles.row} ${className}`)} ref={ref}>
       {columnDataList.map(({ originalIndex }) => childrenArray[originalIndex])}
     </tr>
   );
